af69bf6b2a96e847cddf140b3d780e284642f387
Gary Mulder <gjmulder@gmail.com>
2019-09-12 13:24:48 +0300

    Three feature synth time series

--- synth_dtw_cluster_analysis.R
+++ synth_dtw_cluster_analysis.R
@@ -1,84 +1,89 @@
-# devtools::install_github("ykang/tsgeneration")
-# library(doParallel)
-library(tidyr)
-library(dplyr)
-library(purrr)
-library(tsgeneration)
-library(dtwclust)
-library(clusterCrit)
-library(ggplot2)
-
-set.seed(42)
-
-# TODO:
-# Add single seasonality and deseasonalise using STL
-# Add stochiastic noise
-
-#######################################################################
+# # devtools::install_github("ykang/tsgeneration")
+# # library(doParallel)
+# library(tidyr)
+# library(dplyr)
+# library(purrr)
+# library(tsgeneration)
+# library(dtwclust)
+# library(clusterCrit)
+# library(ggplot2)
 #
-# Config variables
-
-num_ts <- 10
-# vals <- c(2, 3, 5, 7, 11, 13, 17, 19, 23, 29)
-# selected_features <- c('entropy', 'trend', 'seasonal_strength')
-# extra_target <- c(0.6, 0.2, 0.1)
-vals_df <- data.frame("entropy" = c(1:3) / 4, "trend" = c(1:3) / 4)
-targets_df <- as.data.frame(t(expand.grid(vals_df)))
-extra_targets <- NULL
-# ground_truths <- unlist(lapply(1:length(vals), rep, num_ts))
-
-#######################################################################
-
-fname <-
-  paste0(
-    "numts=",
-    num_ts * nrow(targets_df),
-    "_num_feat=",
-    nrow(targets_df),
-    "_feat=",
-    paste0(selected_features, collapse = ","),
-    "tgt=",
-    paste0(extra_targets, collapse = ",")
-  )
-
-#######################################################################
-# Generate synthetic time series
-
-# cl <- makeCluster(2)
-# registerDoParallel(cl)
-
-gen_ts <- function(targets, n) {
-  print(paste0("val=(",
-               paste0(targets, collapse = ","),
-               "), n=",
-               n))
-
-  df_ts <- as.data.frame(
-    generate_ts_with_target(
-      parallel = TRUE,
-      # seed = 42,
-      n = n,
-      ts.length = 100,
-      freq = 12,
-      seasonal = 1,
-      features = c('entropy', 'stl_features'),
-      selected.features = colnames(vals_df),
-      target = c(targets, extra_target)
-    )
-  )
-  names(df_ts) <-
-    paste0("t", paste0(targets, collapse = "-"), "_", c(1:n))
-  return(df_ts)
-}
-
-tsl <- lapply(vals_comb_df, gen_ts, num_ts)
-# # stopCluster(cl)
-# # registerDoSEQ()
-
-ts_df <- do.call("cbind", tsl)
-tsl2 <- as.list(ts_df)
-
-save(tsl2, file = paste0(fname, ".Rdata"))
+# set.seed(42)
+#
+# # TODO:
+# # Add single seasonality and deseasonalise using STL
+# # Add stochiastic noise
+#
+# #######################################################################
+# #
+# # Config variables
+#
+# num_ts <- 10
+# # selected_features <- c('entropy', 'trend', 'seasonal_strength')
+# # extra_target <- c(0.6, 0.2, 0.1)
+# features <- c('entropy', 'stl_features')
+# vals_comb_df <-
+#   data.frame(
+#     "entropy" = c(1:4) / 5,
+#     "trend" = c(0:3) / 4,
+#     "spike" = c(0:3) / 4
+#   )
+# targets_df <- as.data.frame(t(expand.grid(vals_comb_df)))
+# extra_targets <- NULL
+# # ground_truths <- unlist(lapply(1:length(vals), rep, num_ts))
+#
+# #######################################################################
+#
+# fname <-
+#   paste0(
+#     "nts=",
+#     num_ts * ncol(targets_df),
+#     "_nfeat=",
+#     ncol(targets_df),
+#     "_feat=",
+#     paste0(rownames(targets_df), collapse = ","),
+#     "_ext=",
+#     paste0(extra_targets, collapse = ",")
+#   )
+#
+# #######################################################################
+# # Generate synthetic time series
+#
+# # cl <- makeCluster(2)
+# # registerDoParallel(cl)
+#
+# gen_ts <- function(targets, n) {
+#   print(paste0("targets=(",
+#                paste0(targets, collapse = ","),
+#                "), n=",
+#                n))
+#
+#   df_ts <- as.data.frame(
+#     generate_ts_with_target(
+#       parallel = TRUE,
+#       # seed = 42,
+#       n = n,
+#       ts.length = 100,
+#       freq = 12,
+#       seasonal = 1,
+#       features = features,
+#       selected.features = colnames(vals_comb_df),
+#       target = c(targets, extra_targets)
+#     )
+#   )
+#   names(df_ts) <-
+#     paste0("t", paste0(targets, collapse = "-"), "_", c(1:n))
+#   return(df_ts)
+# }
+#
+# tsl <- lapply(targets_df, gen_ts, num_ts)
+# # # stopCluster(cl)
+# # # registerDoSEQ()
+#
+# ts_df <- do.call("cbind", tsl)
+# tsl2 <- as.list(ts_df)
+#
+# save(tsl2, file = paste0(fname, ".Rdata"))
 
 #######################################################################
 # DTW clustering
@@ -94,7 +99,7 @@ int_criteria <- function(traj, vec)
 # cl <- interactive_clustering(tsl2)
 ts_clust_k <- function(k) {
   cl <- tsclust(
-    tsl2,
+    ts_df,
     k = k,
     distance = "L2",
     centroid = "pam",
@@ -111,7 +116,7 @@ ts_clust_k <- function(k) {
 }
 
 # Cluster and Validate
-k_range = c(2:(2 * length(vals)))
+k_range = c(2:(2 * length(targets_df)))
 metrics_k <- lapply(k_range, ts_clust_k)
 # stopCluster(cl)
 # registerDoSEQ()
@@ -135,14 +140,13 @@ int_metrics_df %>%
 
 title <-
   paste0(
-    num_ts * nrow(targets_df),
-    " TS, name=",
-    colnames(targets_df),
-    ", num feats=",
-    nrow(targets_df),
-    paste0(colnames(vals_df), collapse = ","),
-    "), target=(",
-    paste0(extra_target, collapse = ","),
+    num_ts * ncol(targets_df),
+    " TS, uniq feats=",
+    ncol(targets_df),
+    ", targets=(",
+    paste0(rownames(targets_df), collapse = ","),
+    "), extra targets=(",
+    paste0(extra_targets, collapse = ","),
     ")"
   )
 
@@ -150,8 +154,8 @@ gg <-
   ggplot(int_results) +
   ggtitle(title) +
   geom_point(aes(x = k, y = value), size = 0.5) +
-  geom_vline(xintercept = length(vals)) +
-  facet_wrap(~ metric, scales = "free")
+  geom_vline(xintercept = ncol(targets_df)) +
+  facet_wrap( ~ metric, scales = "free")
 
 print(gg)
 ggsave(
